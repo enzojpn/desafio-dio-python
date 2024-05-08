1e7bc5e046429a5b04d8a5ac865783014b560d16
enzojpn <enzo_jpn@hotmail.com>
2024-05-08 10:44:49 -0300

    criando metodos sacar e depositar

--- desafio.py
+++ desafio.py
@@ -13,36 +13,41 @@ extrato = ""
 numero_saque = 0
 LIMITE_SAQUES = 3
 
+def depositar(valor , saldo , extrato):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Deposito = {valor:.2f}\n"
+        print(f"saldo {saldo}" )
+    else:
+        print("valor negativo - deposito invalido")
+    return saldo , extrato
+
+def sacar(saldo ,limite ,extrato, numero_saque , LIMITE_SAQUES ):
+    if numero_saque == LIMITE_SAQUES:
+        print("limite de saque diario estourou")
+    elif valor > 500:
+        print("ultrapassou o limite de 500 reais por saque")
+    elif valor > saldo:
+        print("saldo insuficiente")
+    else:
+        saldo -= valor
+        numero_saque += 1            
+        extrato += f"Saque = {valor:.2f}\n"
+        print(f"valor {saldo}")
+    return saldo , extrato , numero_saque
+
 while True :
     opcao = input(menu)
 
     if opcao == "d":
-
         valor = float(input("digite o valor do deposito: "))
+        saldo , valor = depositar(valor, saldo, extrato)
         
-        if valor > 0:
-            saldo += valor
-            extrato += f"Deposito = {valor:.2f}\n"
-            print(f"valor {saldo}" )
-        else:
-            print("valor negativo - deposito invalido")
 
-    elif opcao == "s":
-        
+    elif opcao == "s": 
         valor = float(input("digite o valor do saque: "))
-        if numero_saque == LIMITE_SAQUES:
-            print("limite de saque diario estourou")
-        elif valor > 500:
-            print("ultrapassou o limite de 500 reais por saque")
-        elif valor > saldo:
-            print("saldo insuficiente")
-        else:
-            saldo -= valor
-            numero_saque += 1            
-            extrato += f"Saque = {valor:.2f}\n"
-            print(f"valor {saldo}")
- 
-
+        saldo , extrato, numero_saque=  sacar(saldo ,limite ,extrato, numero_saque , LIMITE_SAQUES )
+   
     elif opcao == "e":
         print("\n============Extrato Bancario============")
         print("Não existe lancamento no extrato" if not extrato else extrato)
