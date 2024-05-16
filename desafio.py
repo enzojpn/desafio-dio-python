eaa9d793f01bf27d51c2485a269fbd7ced1ae03f
enzojpn <enzo_jpn@hotmail.com>
2024-05-16 10:33:08 -0300

    cadastro de usuario e conta

--- desafio.py
+++ desafio.py
@@ -3,6 +3,9 @@ menu = """
     [d] Depositar
     [s] Sacar
     [e] Extrato
+    [u] Criar usuario
+    [c] Criar conta    
+    [l] Listar contas
     [q] Sair
 
 => """
@@ -12,6 +15,9 @@ limite = 500
 extrato = ""
 numero_saque = 0
 LIMITE_SAQUES = 3
+AGENCIA = "0001"
+usuarios = [] 
+contas  = []
 
 def depositar(valor , saldo , extrato):
     if valor > 0:
@@ -42,6 +48,45 @@ def consultar_extrato():
     print(f"SALDO TOTAL:  R$ {saldo:.2f}")
     print("\n============END============")
 
+def cadastrar_usuario(usuarios):
+    cpf = input("informe o CPF somente numeros: ")
+    usuario = filtrar_usuario(cpf , usuarios)
+
+    if usuario:
+        print("usuario ja cadastrado com este cpf")
+        return
+    
+    nome = input("digite o nome: ")
+    data_nascimento = input("digite a data de nascimento (dd-mm-aaa): ")
+    endereco = input("digite o endereco (logradouro , numero - bairro - municipio e UF ): ")
+
+    usuarios.append({"nome":nome, "data_nascimento": data_nascimento , "cpf": cpf , "endereco": endereco})
+    print("usuario criado com sucesso!!" )
+
+def filtrar_usuario(cpf , usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario['cpf'] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def criar_conta(agencia, numero_conta , usuario):
+    cpf = input("informe o CPF somente numeros: ")
+    usuario = filtrar_usuario(cpf , usuarios)
+
+    if usuario:
+        print("conta criada com sucesso!!")
+        return {"agencia" : agencia, "numero_conta": numero_conta , "usuario" : usuario}
+
+    print("usuario nao encontrado, favor cadastrar usuario !!")
+
+def listar_contas():
+    for conta in contas:
+        linha = f"""\
+        Agencia:\t{conta['agencia']} 
+        C/C:\t{conta['numero_conta']}       
+        Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(linha)
+
 while True :
     opcao = input(menu)
 
@@ -56,6 +101,21 @@ while True :
    
     elif opcao == "e":
         consultar_extrato()
+    
+    elif opcao == "u":
+        cadastrar_usuario(usuarios)
+
+    elif opcao == "c":
+        numero_conta = len(contas) + 1
+        conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+        if conta:
+            contas.append(conta)
+
+    elif opcao == "l":
+        print("lista de contas")
+        listar_contas()
+
     elif opcao == "q":
         break
     else:
